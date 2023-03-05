434d45d55056096208ab6ea26e17266dcec8d307
Kwadwo Addo <kwadwoaddo97@gmail.com>
2023-03-05 00:05:22 +0000

    implementing xray

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -33,16 +33,16 @@ provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
-# Initialize AWS X-ray
-xray_url = os.getenv("AWS_XRAY_URL")
-xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
-
 app = Flask(__name__)
 
 # Honeycomb
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
+
+# Initialize AWS X-ray
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)
 
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
