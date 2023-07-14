947630f19b35e174521eabf09e4fa3f8045f8ab9
Kwadwo Addo <kwadwoaddo97@gmail.com>
2023-07-14 05:45:33 +0000

    bug fixes

--- frontend-react-js/src/components/ActivityFeed.js
+++ frontend-react-js/src/components/ActivityFeed.js
@@ -5,18 +5,19 @@ export default function ActivityFeed(props) {
   let content;
   if (props.activities.length === 0){
     content = <div className='activity_feed_primer'>
-      <span> Nothing to see yet. Check back later!!</span>
+      <span>Nothing to see here yet</span>
+    </div>
+  } else {
+    content = <div className='activity_feed_collection'>
+      {props.activities.map(activity => {
+      return  <ActivityItem setReplyActivity={props.setReplyActivity} setPopped={props.setPopped} key={activity.uuid} activity={activity} />
+      })}
     </div>
   }
 
-  else {
-    content = <div className='activity_feed_collection'>
-    {props.activities.map(activity => {
-    return  <ActivityItem setReplyActivity={props.setReplyActivity} setPopped={props.setPopped} key={activity.uuid} activity={activity} />
-    })}
-  </div>
-  }
+
   return (<div>
     {content}
-    </div>);
+  </div>
+  );
 }
