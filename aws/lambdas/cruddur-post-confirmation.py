766a4f9cacaf7d31e01c1a04c0c7e4b15dfe4cf9
Kwadwo Addo <kwadwoaddo97@gmail.com>
2023-07-16 17:20:41 +0000

    journaling work in progress

--- aws/lambdas/cruddur-post-confirmation.py
+++ aws/lambdas/cruddur-post-confirmation.py
@@ -21,23 +21,17 @@ def lambda_handler(event, context):
           cognito_user_id
           ) 
         VALUES(
-        %s,
-        %s,
-        %s,
-        %s
+          '{user_display_name}', 
+          '{user_email}', 
+          '{user_handle}', 
+          '{user_cognito_id}'
         )
       """
       print('SQL Statement ----')
       print(sql)
       conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
       cur = conn.cursor()
-      parameters = [
-        user_display_name,
-        user_email,
-        user_handle,
-        user_cognito_id
-        ]
-      cur.execute(sql,*parameters)
+      cur.execute(sql)
       conn.commit() 
 
     except (Exception, psycopg2.DatabaseError) as error:
